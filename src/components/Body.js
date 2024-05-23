e9d81e5b09f01643abfaf168cb85f36ee47c4de0
adnan0758 <21131a4456@gvpce.ac.in>
2024-05-23 18:27:41 +0530

    first commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import Restocard from "./RestoCard";
 import { useState,useEffect} from "react";
-import  resobj from "../utils/mockdata";
+import resobj from "../utils/mockdata";
 
 const Body = () => {
     const [lsres,setlsres] = useState(resobj);
@@ -9,11 +9,11 @@ const Body = () => {
         fetchData();
     },[]);
 const fetchData=async()=>{
-    const data=await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=17.93249124556291&lng=83.42679720371962");
+    const data=await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=17.8176974&lng=83.3503512");
     const json= await data.json();
     console.log(json);
-    setlsres(json.data.cards);
-};
+    setlsres(json.data.success.cards);
+}
     return (
       <div className="body-container">
         <div className="filter">
@@ -26,11 +26,11 @@ const fetchData=async()=>{
         </div>
         <div className="res-container">
           {lsres.map((restaurant)=>(
-            <Restocard key={restaurant.info.id} resdata={restaurant} />
+            <Restocard key={restaurant.id} resdata={restaurant} />
           ))}
         </div>
       </div>
     );
   };
 
-  export default Body
+  export default Body;
--- src/components/RestoCard.js
+++ src/components/RestoCard.js
@@ -2,20 +2,20 @@ import { CDN_URL } from "../utils/constants";
 
 const Restocard = ({ resdata }) => {
     const { name, avgRating, cuisines, sla } = resdata.info;
-  
+
     return (
-     
+
       <div className="res-card">
         <div className="res-logo">
           <img className="logo" src={CDN_URL+resdata.info.cloudinaryImageId} />
-         
+
           <h3>{name}</h3>
           <h4>Rating: {avgRating}</h4>
           <h4>{cuisines.join(", ")}</h4>
           <h4>{sla.slaString}</h4>
         </div>
       </div>
-     
+
     );
   }; 
 
